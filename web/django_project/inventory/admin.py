c411a3a6c7f6cd75b49a53d7ce1a00990fad0d7f
Aymane <aymane@example.com>
2026-02-27 17:58:38 +0000

    last update

--- web/django_project/inventory/admin.py
+++ web/django_project/inventory/admin.py
@@ -1,2 +1,8 @@
+from django.contrib import admin
+from .models import Product, Customer, Order, OrderItem
 
 
+admin.site.register(Product)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderItem)
