5460747d776cffcae1202a0f6d0ebff9c348e030
Aymane <aymane@example.com>
2026-02-27 16:43:50 +0000

    last update

--- web/django_project/inventory/admin.py
+++ web/django_project/inventory/admin.py
@@ -1,21 +1,2 @@
-from django.contrib import admin
-from .models import Product, Customer, Order, OrderItem
 
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'price', 'quantity_in_stock')
-
-@admin.register(Customer)
-class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email')
-
-@admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'customer', 'order_date')
-
-
-@admin.register(OrderItem)
-class OrderItemAdmin(admin.ModelAdmin):
-    list_display = ('order', 'product', 'quantity')
 
